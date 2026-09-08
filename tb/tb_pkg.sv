6f36c0a36061b5dd253d5dbdf58c1cb038753fc8
yonizvida-gif <yonizvida@gmail.com>
2026-09-08 14:17:11 +0300

    Update tb_pkg.sv

--- tb/tb_pkg.sv
+++ tb/tb_pkg.sv
@@ -1,7 +1,7 @@
 package tb_pkg; // 1. name
    	 import uvm_pkg::*; // 2. basic package
 	
-    	`include "uvm_macros.svh" // 3. macro package
+    `include "uvm_macros.svh" // 3. macro package
 	
 	`include "transaction.sv"
 	`include "header_at_end.sv"
@@ -16,15 +16,15 @@ package tb_pkg; // 1. name
 	`include "seq_header_in_payload_2.sv"
 	`include "seq_random.sv"
 	`include "master_seq.sv"
-    	`include "sequencer.sv"
-    	`include "driver.sv"
+    `include "sequencer.sv"
+    `include "driver.sv"
 	`include "monitor_in.sv"
 	`include "monitor_out.sv"
 	`include "agent_in.sv"
 	`include "agent_out.sv"
 	`include "scoreboard.sv"
 	`include "coverage.sv"
-    	`include "environment.sv"
-    	`include "test.sv"
+    `include "environment.sv"
+    `include "test.sv"
     
 endpackage 
